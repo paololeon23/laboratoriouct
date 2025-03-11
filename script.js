ef19246af678cfb98ae8be1e32e2476a82cf745f
paololeon23 <pleona1@upao.edu.pe>
2025-03-11 13:05:25 -0500

    first commit

---
+++ script.js
@@ -0,0 +1,7 @@
+document.getElementById("protocolo-btn").addEventListener("click", function() {
+    window.location.href = "./PDF/Protocolo.pdf"; // Cambiar al path correcto del PDF
+});
+
+document.getElementById("manual-btn").addEventListener("click", function() {
+    window.location.href = "./PDF/Manual.pdf"; // Cambiar al path correcto del PDF
+});
